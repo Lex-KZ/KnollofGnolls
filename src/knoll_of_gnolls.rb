a45b51a3a5ae2cce49397385d53874146ed30340
Lex-KZ <azaskalkina@hotmail.com>
2021-04-06 10:47:32 +1000

    Initial commit after first push

---
+++ src/knoll_of_gnolls.rb
@@ -0,0 +1,65 @@
+
+puts "The Knoll of Gnolls"
+
+sleep 1
+
+puts "Hello adventurer!"
+
+sleep 1
+
+puts "Welcome to the world of Otira."
+
+sleep 1
+
+puts "What would you like to do? (new, load, delete, help)"
+
+####################################################################
+puts "What is your name, adventurer?"
+name = gets.chomp
+
+puts "Hello, #{name}"
+
+sleep 1
+
+puts "On the 23rd day of Phiots, in the year 1238 the"
+puts "sun was beating down on the small town of Ruesin."
+
+sleep 3
+
+puts "You arrived in town that morning, eager for work"
+puts "and gold."
+
+sleep 3
+
+puts "You heard that some nights prior a pack of gnolls,"
+puts "hyena-headed humanoids, had raided the town and" 
+puts "had taken three citizens."
+
+sleep 4
+
+puts "You've never encountered gnolls before, but you"
+puts "know they are very aggressive, especially when"
+puts "they're hungry."
+
+sleep 4
+
+puts "The town offered 400 gold pieces to anyone brave"
+puts "enough to venture into the Blazing Jungles that"
+puts "surround the town and retrieve the three missing,"
+puts "either by bringing them back safely or by bringing"
+puts "back their bodies for burial."
+
+sleep 6
+
+puts "You, being certain of your skills and knowledge of" 
+puts "nature had taken up the contract."
+
+sleep 3
+
+puts "The distant caw of a jungle bird brought you back to"
+puts "the here and now."
+
+sleep 3
+
+puts "Please pick a Class: (Ranger, Sorcerer, what is this?"
+
